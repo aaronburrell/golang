a59600dd75a5df840c2784c3780ddd167b9a4552
Aaron Burrell <burrelljr_aaron@bah.com>
2015-09-10 09:35:22 -0400

    Made a few updates that allow me to test out my db object

--- router/main.go
+++ router/main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 
     router := NewRouter()
-
+  //  db := Open()
+  //  CreateBucket(db, "MyBucket")
+  //  SaveBucket(db,"MyBucket", "test", "I did it!")
+  //  ReadBucket(db,"MyBucket","test")
     log.Fatal(http.ListenAndServe(":8080", router))
 }
