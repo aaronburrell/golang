9f73a7c254d8ae5fbc746ed5c71d136a43ccafb7
Aaron Burrell <burrelljr_aaron@bah.com>
2015-09-14 10:21:57 -0400

    Updated code to allow the concept of using auth redirecting from any path until the user is authenticated.

--- revelapp/app/controllers/Group.go
+++ revelapp/app/controllers/Group.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"gitprojects/golang/revelapp/app/models"
+	"log"
 )
 type Group struct {
 	*revel.Controller
@@ -10,9 +11,11 @@ type Group struct {
 
 func (c Group) Group() revel.Result {
 	u := c.connected()
+	log.Println("HERE")
 	if u != nil && u.AccessToken != "" {
 	return c.Render()
 	}
+	c.Session["redirect"] = "/groups"
 	return c.Redirect(App.Index)
 }
 
