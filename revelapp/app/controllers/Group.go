f0af2caaacd522a4ad5bfb329ca80e883a5f7181
Aaron Burrell <burrelljr_aaron@bah.com>
2015-09-14 09:17:19 -0400

    Added the ability to force auth on the /groups

--- revelapp/app/controllers/Group.go
+++ revelapp/app/controllers/Group.go
@@ -1,11 +1,25 @@
 package controllers
 
-import "github.com/revel/revel"
-
+import (
+	"github.com/revel/revel"
+	"gitprojects/golang/revelapp/app/models"
+)
 type Group struct {
 	*revel.Controller
 }
 
 func (c Group) Group() revel.Result {
+	u := c.connected()
+	if u != nil && u.AccessToken != "" {
 	return c.Render()
+	}
+	return c.Redirect(App.Index)
+}
+
+func init() {
+	revel.InterceptFunc(setuser, revel.BEFORE, &Group{})
+}
+
+func (c Group) connected() *models.User {
+	return c.RenderArgs["user"].(*models.User)
 }
